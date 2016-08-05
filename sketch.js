1d376ac1b51007d8c97c61159d1bde937f76fa72
Micaela Millan <mmillan014@gmail.com>
2016-08-05 15:16:58 -0400

    drawing program

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var myred = 242;
 var mygreen = 90;
 var myblue = 250;
 
+
 function circle(x, y, diameter) {
 ellipse(x, y, diameter, diameter);
 }
@@ -12,12 +13,33 @@ function setup() {
   }
 
 function draw() {
-  myblue = myblue - .25;
-  mygreen = mygreen - .25;
-  myred = myred - .25;
-  fill (myred, mygreen, myblue);
-  noStroke();
-  circle(mouseX, mouseY, 10);
+fill(myred, mygreen, myblue);
+noStroke();
+circle(mouseX, mouseY, 20, 20);
+
+myred = myred - .5;
+  if (myred > 255) {
+    myred = 0;
+  }
   
- 
+myblue = myblue - .5;
+  if (myblue <= 0) {
+    myblue = 255;
+  }
+
+mygreen = mygreen - .5;
+  if (mygreen <= 0) {
+    mygreen = 255;
+  }
+if (mouseIsPressed){
+  noStroke();
+  fill(255, 255, 255);
+  ellipse(mouseX, mouseY, 25, 25);
+  if(mouseIsPressed == false){
+  fill(myred, mygreen, myblue);
+  }
 }
+}
+  
+
+  
