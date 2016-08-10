3f3e87a7bdde2d36017f26e8aed6815c5da13b36
Micaela Millan <mmillan014@gmail.com>
2016-08-10 09:51:35 -0400

    hello

--- sketch.js
+++ sketch.js
@@ -10,6 +10,20 @@ ellipse(x, y, diameter, diameter);
 function setup() {
   createCanvas(640, 480);
   background(255);
+  strokeWeight(4);
+  stroke(0);
+
+var x = 0;
+while (x <= width) {
+  fill(204, 255, 0);
+  ellipse (x, 100, 50, 25);
+  x = x + 25;
+}
+for (var x = 0; x <= width; x = x +50) {
+  fill(230, 41, 16);
+  ellipse(x, 300, 75, 50);
+}
+
   }
 
 function draw() {
@@ -34,9 +48,10 @@ mygreen = mygreen - .5;
 if (mouseIsPressed){
   noStroke();
   fill(255, 255, 255);
-  ellipse(mouseX, mouseY, 25, 25);
+  ellipse(mouseX, mouseY, 35, 25);
   if(mouseIsPressed == false){
   fill(myred, mygreen, myblue);
+  
   }
 }
 }
